e8e694bada062c3daf29933b2fb3f61130eeef05
Sukumu <48925529+Sukumu@users.noreply.github.com>
2021-08-27 14:30:18 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,9 @@ export default function Layout({ children, pageTitle, description, ...props }) {
             <link rel="webmention" href="https://webmention.io/sukumu.com/webmention" />
             <link rel="pingback" href="https://webmention.io/sukumu.com/xmlrpc" />
             <link rel="webmention" href="https://webmention.io/webmentions-next.netlify.app/webmention" />
-            <link rel="pingback" href="https://webmention.io/webmentions-next.netlify.app/xmlrpc" />    
+            <link rel="pingback" href="https://webmention.io/webmentions-next.netlify.app/xmlrpc" />
+            <link rel="authorization_endpoint" href="https://indieauth.com/auth">
+            <link rel="token_endpoint" href="https://tokens.indieauth.com/token">
         <title>{pageTitle}</title>
       </Head>
       <style jsx global>{`
