bde7f164e753610548e4fc9b2fa7e1535eef6b81
Sukumu <48925529+Sukumu@users.noreply.github.com>
2021-08-27 14:14:18 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ const Index = ({ posts, title, description, ...props }) => {
   const [mentions, setMentions] = useState([]);
   useEffect(() => {
     fetch(
-   'https://webmention.io/api/mentions.jf2?domain=webmentions-next.netlify.app&token=7EYmT-ELqtcIrIxRJmaj3Q',
-  
+           'https://webmention.io/api/mentions.html?token=lWeKyI6a61dVQGfRLWfUpg',  
     )
       .then((response) => response.json())
       .then((result) => {
