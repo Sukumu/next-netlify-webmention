deec8e13acfd29c0d066cbe26880f3650761bd80
Sukumu <48925529+Sukumu@users.noreply.github.com>
2021-08-27 08:49:23 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ export default function Header() {
     
           <a href="https://twitter.com/sukumu4" rel="me"> Twitter sukumu </a>
           <a href="https://www.linkedin.com/in/simba-gentry-99267b71/" rel="me"> LinkedIn </a>
-           
+           <a href="https://medium.com/@sukumu73" rel="me"> Medium </a>           
         </nav>
       </header>
       <style jsx>{`
