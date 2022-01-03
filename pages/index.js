8312d284a8753f159fd0b84541879b5848dbd706
Sukumu <48925529+Sukumu@users.noreply.github.com>
2022-01-03 14:48:29 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -67,8 +67,8 @@ const Index = ({ posts, title, description, ...props }) => {
             </div>
           ))}
         </div>
-      </Layout>
-     <iframe style="width:100%;" data-anvil-embed src="https://Z2WEDKIJP4DIZ3I5.anvil.app/ZB7FMUJN3TTGJ2O544UPCXNW"></iframe>
+ <iframe style="width:100%;" data-anvil-embed src="https://Z2WEDKIJP4DIZ3I5.anvil.app/ZB7FMUJN3TTGJ2O544UPCXNW"></iframe>
+      </Layout>     
       <style jsx>{`
         .title {
           margin: 1rem auto;
